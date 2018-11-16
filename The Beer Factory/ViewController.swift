7ec8a92815d59ed6dfff70e9f338d4467e1680a0
Zin Lin Phyo <zinlin.phyo@mm-digital-solutions.com>
2018-11-16 22:34:13 +0630

    Initial Commit

---
+++ The Beer Factory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Beer Factory
+//
+//  Created by Zin Lin Phyo on 11/16/18.
+//  Copyright © 2018 Zin Lin Phyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
